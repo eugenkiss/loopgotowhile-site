90cd42ef03184b0f2cd3837795e42e072d498f1c
Eugen Kiss <eugen@eugenkiss.com>
2011-06-06 16:42:38 +0200

    Initial commit

---
+++ LGWServer.hs
@@ -0,0 +1,138 @@
+import System.Timeout (timeout)
+import Control.Concurrent.MVar
+import Control.Monad.Trans (liftIO)
+import Control.Monad (msum)
+import Control.Applicative ((<$>), (<*>))
+import Data.Maybe (listToMaybe, fromMaybe)
+import Data.List.Split (splitOn)
+
+import qualified Happstack.Server as H
+import Happstack.Server 
+    ( Response, ServerPart, Method(POST), BodyPolicy(..)
+    , decodeBody, defaultBodyPolicy 
+    , dir, dirs, nullDir, anyPath
+    , serveFile, asContentType 
+    , nullConf, ok, simpleHTTP, toResponse, methodM, badRequest, look
+    ) 
+import Happstack.Server.RqData (RqData)
+
+import qualified Language.LoopGotoWhile.Loop.Strict     as LoopS
+import qualified Language.LoopGotoWhile.Loop.Extended   as LoopE
+import qualified Language.LoopGotoWhile.Loop.Transform  as LoopT
+import qualified Language.LoopGotoWhile.Goto.Strict     as GotoS
+import qualified Language.LoopGotoWhile.Goto.Extended   as GotoE
+import qualified Language.LoopGotoWhile.Goto.Transform  as GotoT
+import qualified Language.LoopGotoWhile.While.Strict    as WhileS
+import qualified Language.LoopGotoWhile.While.Extended  as WhileE
+import qualified Language.LoopGotoWhile.While.Transform as WhileT
+
+
+main :: IO ()
+main = do
+    mvar <- newEmptyMVar
+    simpleHTTP myConf $ handlers mvar
+
+-- The time limit for computations in ms.
+timeLimit :: Int
+timeLimit = 5000
+
+myConf :: H.Conf
+myConf = H.Conf 
+    { H.port      = 8000
+    , H.validator = Nothing
+    , H.logAccess = H.logAccess nullConf
+    , H.timeout   = 10
+    }
+
+handlers :: MVar () -> ServerPart Response
+handlers mvar = msum
+    [ dirs "loop/strict"    $ runCode mvar LoopS.run
+    , dirs "loop/extended"  $ runCode mvar LoopE.run
+    , dirs "goto/strict"    $ runCode mvar GotoS.run
+    , dirs "goto/extended"  $ runCode mvar GotoE.run
+    , dirs "while/strict"   $ runCode mvar WhileS.run
+    , dirs "while/extended" $ runCode mvar WhileE.run
+
+    , dirs "loop/to/strict"  $ transformCode mvar $ \s ->
+          LoopE.parse s >>= return . show . LoopT.toStrict 
+    , dirs "loop/to/loop"    $ transformCode mvar $ \s ->
+          LoopE.parse s >>= return . show 
+    , dirs "loop/to/goto"    $ transformCode mvar $ \s ->
+          LoopE.parse s >>= return . show . LoopT.toGoto
+    , dirs "loop/to/while"   $ transformCode mvar $ \s ->
+          LoopE.parse s >>= return . show . LoopT.toWhile
+    , dirs "goto/to/strict"  $ transformCode mvar $ \s ->
+          GotoE.parse s >>= return . show . GotoT.toStrict
+    , dirs "goto/to/goto"    $ transformCode mvar $ \s ->
+          GotoE.parse s >>= return . show 
+    , dirs "goto/to/while"   $ transformCode mvar $ \s ->
+          GotoE.parse s >>= return . show . GotoT.toWhile
+    , dirs "while/to/strict" $ transformCode mvar $ \s ->
+          WhileE.parse s >>= return . show . WhileT.toStrict
+    , dirs "while/to/while"  $ transformCode mvar $ \s ->
+          WhileE.parse s >>= return . show
+    , dirs "while/to/goto"   $ transformCode mvar $ \s ->
+          WhileE.parse s >>= return . show . WhileT.toGoto
+    , anyPath $ dir "to" $ anyPath $ 
+          badRequest $ toResponse "Impossible transformation!"
+
+    {-, nullDir >> serveFile (asContentType "text/html") "index.html"-}
+    {-, dir "style.css" $ serveFile (asContentType "text/css") "style.css"-}
+    , badRequest $ toResponse "Nothing here!"
+    ]
+
+myPolicy :: BodyPolicy
+myPolicy = (defaultBodyPolicy "/tmp/" 0 100000 1000)
+
+runCode :: MVar () -> (String -> [Integer] -> Either String Integer) -> ServerPart Response
+runCode mvar runner = do 
+    methodM POST
+    decodeBody myPolicy
+    source    <- look "source"
+    arguments <- look "args"
+    case parseArgs arguments of
+      Left s     -> badRequest $ toResponse s
+      Right args -> do
+        wasFree <- liftIO $! tryPutMVar mvar ()
+        liftIO $! print wasFree
+        if wasFree then do
+           result <- liftIO $! timeout (1000 * timeLimit) $! do
+               let s = either id show $ runner source args
+               print s -- WTF!!! do I need this line so that *** timeout works!?!?!
+               return s
+           temp <- liftIO $! takeMVar mvar
+           liftIO $! print temp
+           case result of
+             Nothing -> badRequest $ toResponse "Computation took too long!"
+             Just x  -> ok $ toResponse x
+           else badRequest $ toResponse "Server is busy!"
+
+transformCode :: MVar () -> (String -> Either String String) -> ServerPart Response
+transformCode mvar runner = do 
+    methodM POST
+    decodeBody myPolicy
+    source <- look "source"
+    wasFree <- liftIO $! tryPutMVar mvar ()
+    liftIO $! print wasFree
+    if wasFree then do
+       result <- liftIO $! timeout (1000 * timeLimit) $! do
+           let s = either id id $ runner source
+           print s -- WTF!!! do I need this line so that *** timeout works!?!?! 
+           return s
+       temp <- liftIO $! takeMVar mvar
+       liftIO $! print temp
+       case result of
+         Nothing -> badRequest $ toResponse "Computation took too long!"
+         Just x  -> ok $ toResponse x
+       else badRequest $ toResponse "Server is busy!"
+
+parseArgs :: String -> Either String [Integer]
+parseArgs "" = Right []
+parseArgs s  = if any (== Nothing) result 
+                  then Left "Parse Error on Arguments!"
+                  else Right $ map (fromMaybe 0) result
+  where result = map maybeRead . splitOn "," . trimWhiteSpace $ s
+        trimWhiteSpace = filter (/= ' ')
+
+maybeRead :: Read a => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . filter (null . snd) . reads
---
+++ fabfile.py
@@ -0,0 +1,58 @@
+from fabric.api import *
+import os, os.path
+import fabric.contrib.project as project
+
+PROD = 'root@eugenkiss.com:223'
+DEST_PATH = '/var/www/loopgotowhile/'
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
+NGINX_CONF_PATH = '/etc/nginx/sites-available/loopgotowhile'
+
+@hosts(PROD)
+def update_nginx_conf():
+    put('nginx.conf', NGINX_CONF_PATH)
+    run('/etc/init.d/nginx reload')
+
+def backup():
+    local('git push ~/Dropbox/backup/loopgotowhile-site.git')
+
+def compile():
+    local('cabal configure')
+    local('cabal build')
+
+def copy():
+    # make sure the directory is there!
+    local('mkdir -p ' + DEPLOY_PATH)
+    local('cp ' + os.path.join(ROOT_PATH, 'index.html') + ' ' +
+                  os.path.join(DEPLOY_PATH, 'index.html'))
+    local('cp ' + os.path.join(ROOT_PATH, 'style.css') + ' ' +
+                  os.path.join(DEPLOY_PATH, 'style.css'))
+    local('cp ' + os.path.join(ROOT_PATH, 'dist/build/LGWServer/LGWServer') + ' ' +
+                  os.path.join(DEPLOY_PATH, 'LGWServer'))
+
+@hosts(PROD)
+def publish():
+    # Kill old LGWServer instances
+    # Note: For some magical reason I have to kill the processes here instead
+    # of directly before restarting them at the bottom of this function. 
+    # Otherwise LGWServer is hardly ever started on the remote. I guess the
+    # killall has some delay/aftermath... but I also tried to put a `sleep 5s`
+    # between the kills and the restarts without any luck...
+    with settings(warn_only=True):
+        run('killall -9 -v LGWServer ')
+        run('killall -9 -v cpulimit ')
+    #backup()
+    compile()
+    copy()
+    project.rsync_project(
+        remote_dir=DEST_PATH,
+        local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        delete=True
+    )
+    update_nginx_conf()
+    # Limit cpu usage (cpulimit must be installed on remote)
+    run('nohup cpulimit -e LGWServer -l 20 >& /dev/null < /dev/null &')
+    run('sleep 1s')
+    # Run LGWServer in background and limit heap size
+    run('nohup ' + os.path.join(DEST_PATH, 'LGWServer') + ' +RTS M30m -RTS >& /dev/null < /dev/null &')
+    run('sleep 1s')
